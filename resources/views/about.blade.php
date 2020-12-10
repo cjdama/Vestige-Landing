0da73767ac0d397d50381a3bfedf83e9cff92c84
cjdama <damacrisciait1b@gmail.com>
2020-12-10 08:50:15 +0800

    'committed'

---
+++ resources/views/about.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vestige Infinity Landing Page</title>
+    <link rel="stylesheet" href="assets/vendors/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header class="foi-header">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light foi-navbar">
+                <a class="navbar-brand" href="index.html">
+                    <img src="assets/images/logo.png" alt="FOI">
+                </a>
+                <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="about.html">About <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="features.html">Features</a>
+                        </li>
+                       
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">contact</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav mt-2 mt-lg-0">
+                        <li class="nav-item mr-2 mb-3 mb-lg-0">
+                            <a class="btn btn-secondary" href="register.html">Sign up</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-secondary" href="login.html">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <main class="page-about">
+        <div class="container">
+            <section class="page-header">
+                <h1>About</h1>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb foi-breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">About</li>
+                    </ol>
+                </nav>
+            </section>
+            <section class="foi-page-section pt-0">
+                <div class="row">
+                    <div class="col-md-6 mb-4 mb-md-0 pr-lg-0">
+                        <h2 class="about-section-one-title">
+                            Mission and Vision
+                        </h2>
+                        <div class="about-section-one-content">
+                            <p>Our Vision is to provide people a definite and accurate source of income that will help them to have the best future. We aim to be superior on all products that will improve the beauty and health of the consumer. We also aim to be the most fast growing company that builds successful entrepreneurs.</p>
+                        </div>
+
+                        
+
+
+                        <h2 class="about-section-one-title">
+                            Mission
+                        </h2>
+
+                         <div class="about-section-one-content">
+                            <p>It is our passion to empower all entrepreneurs to pursue long term goals and to be successful via proper guidance, unwavering commitment  to building relationships that deliver results, by the help of the unique compensation plan of the company , and superior products together we will take the path to success.</p>
+                        </div>
+
+                    </div>
+                    <div class="col-md-6 pl-lg-0 d-flex align-items-center align-items-lg-end">
+                        <img src="assets/images/about_img_1.png" alt="about" class="img-fluid" width="448px">
+                    </div>
+                </div>
+            </section>
+            <section class="foi-page-section">
+                <div class="row">
+                    <div class="col-md-6 mb-5 mb-md-0">
+                        <img src="assets/images/about_img_2.png" alt="about 2" class="w-100 img-fluid pr-md-5" width="437px">
+                    </div>
+                    <div class="col-md-6">
+                        <h2 class="about-section-two-title">Purpose</h2>
+                        <div class="about-section-two-content">
+                            <p>The purpose of the company is to make consumers healthy and beautiful and to build successful entrepreneurs.</p>
+                        </div>
+
+                        <h2 class="about-section-two-title">Core Values</h2>
+                        <div class="about-section-two-content">
+                            <p><b> Bravery, </b><span class="font-weight-normal">which springs from natural creativity and a determined spirit, helps the entrepreneur deal with discouragement and even defeat in the early stages of company formation.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <p><b> Vision. </b><span class="font-weight-normal">Vision guides the entrepreneur through the business planning process, clarifying the opportunity and setting goals for the organization. Vision allows the entrepreneur to see past his or her current position and beyond limited resources to more fully appreciate the potential of the venture.</p>
+                        <p><b> Respect </b><span class="font-weight-normal">for self and others. Respect for one’s self and others is essential. It enables the individual to appreciate his or her efforts, relate effectively to family and community, and motivate others. This respect, which stems from pride, hope, and enthusiasm, frees entrepreneurs from the obstacles of low self-esteem, hopelessness, and anger.</p>
+                        <p><b> Trust. </b><span class="font-weight-normal">Trust is an indispensable part of the marketing effort as the company grows. Trust, which results from reliability, compassion, and gentleness, permits the entrepreneur to overcome mistrust, selfishness, and ruthlessness as the company deals with employees, customers, and vendors.</p>
+                        <p><b> Generosity. </b><span class="font-weight-normal">In directing the management and operations of a growing business, an entrepreneur should be generous. Generosity, which develops from supporting and leading others, permits an entrepreneur to eliminate racial bias, reduce resistance to change, and heal dysfunctions within the organization.</p>
+                        <p><b> Fortitude. </b><span class="font-weight-normal">An entrepreneur requires fortitude to keep a business strong and eventually bring it to harvest. Fortitude, which stems from persistence, realism, and consistency, strengthens the entrepreneur against scattered thinking and giving up.</p>
+                </div> </div>
+            </section>
+        </div>
+    </main>
+
+   <footer class="border-top">
+        <br>
+        <p class="text-center text-muted pt-4">Copyright © 2020<a href="https://www.facebook.com/vestigeinfinity/" class="px-1">@vestigeinfinity</a>All rights reserved.</p>
+        <br>
+      </footer>
+     <script src="assets/vendors/jquery/jquery.min.js"></script>
+    <script src="assets/vendors/popper.js/popper.min.js"></script>
+    <script src="assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vestige Infinity Landing Page</title>
+    <link rel="stylesheet" href="assets/vendors/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header class="foi-header landing-header">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light foi-navbar">
+                <a class="navbar-brand" href="index.html">
+                    <img src="assets/images/logo.png" alt="FOI">
+                </a>
+                <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#features-section">Features</a>
+                        </li>
+                   
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">contact</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav mt-2 mt-lg-0">
+                        <li class="nav-item mr-2 mb-3 mb-lg-0">
+                            <a class="btn btn-secondary" href="https://www.vestigeinfinity.com/register?sponsor=owner">Sign up</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-secondary" href="login.html">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="header-content">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1>Stay Home, Stay Safe</h1>
+                        <p class="text-dark">Earn money even at home, be our distributor now, and we will take care of your business. Together we will take the path to success!</p>
+                        <button class="btn btn-primary mb-4">Get Started</button>
+                        <div class="my-2">
+                            <p class="header-app-download-title"> </p>
+                        </div>
+     
+                    </div>
+                  
+                    <div class="embed-responsive embed-responsive-16by9 col-md-6 mb-5">
+                         <iframe src="https://www.youtube.com/embed/ETRZeCvKj6Q?autoplay=1&mute=1" class="embed-responsive-item">
+                         </iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="py-5 mb-5" id="features-section">
+        <div class="container">
+            <h2 class="section-title">Website Features</h2>
+            <br>
+            <div class="row">
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                    <h5>Secure Data Management</h5>
+                    <p class="text-dark">Uses cookies so that we can provide you with the best user experience possible. Cookie information is stored in your browser and performs functions such as recognising you when you return to our website and helping our team to understand which sections of the website you find most interesting.</p>
+                    <br>
+                    <h5>Fully functional</h5>
+                    <p class="text-dark">Working correctly or as expected in every way. It will be very convenient to use and very Friendly-user.</p>
+                    
+                </div>
+                <div class="col-lg-4 mb-3 mb-lg-0">
+                    <h5>Live Chat</h5>
+                    <p class="text-dark">We have a very hands on admins and customer service that you can contact. You will be Redirected to the Official facebook page and chat us at your most convenient time.</p>
+                    <br>
+                    <h5>Powerful dashboard</h5>
+                    <p class="text-dark">Even at home you can increase your income by the power of great compensation plan made just for you.</p>  
+                </div>
+                <div class="col-lg-4">
+                    <h6 class="text-gray font-os font-weight-semibold">Trusted by the world's best</h6>
+                    <div id="landingClientCarousel" class="carousel slide landing-client-carousel" data-ride="carousel">
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                                <div class="d-flex flex-wrap justify-content-center">
+                                    <div class="clients-logo">
+                                        <img src="assets/images/clients/slack.svg" alt="Slack" class="img-fluid">
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="d-flex flex-wrap justify-content-center">
+                                    <div class="clients-logo">
+                                        <img src="assets/images/clients/collagen.svg" alt="Slack" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            
+                        </div>
+                        <ol class="carousel-indicators">
+                            <li data-target="#landingClientCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#landingClientCarousel" data-slide-to="1"></li>
+                        
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-6 mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mb-5 mb-md-0">
+                    <img src="assets/images/app_2.png" alt="special offers" class="img-fluid" width="492px">
+                </div>
+                
+                <div class="col-md-6">
+                    <h2 class="section-title">Get a Lifetime Special offer</h2>
+                    <br>
+                    <p class="mb-5">We had led a remarkable campaign, defying the traditional mainstream business courtesy of direct selling and e-commerce. For many, however, the campaign has become a risky business because there are a lot of circumstances happened online including scams, but we made sure to secure your business with us the help of our website features and hands on customer service. This business will surely help you to achieve your goals and help you to have income even in the comfort of your home. Together we will take the path to Success!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+        <div class="container">
+        <div class="row">
+                <div class="col-md-6 mb-3">
+                    <div class="media landing-feature">
+                        <span class="landing-feature-icon"></span>
+                        <div class="media-body">
+                            <h5>Essentials</h5>
+                            <p>25-50% discounts on all products. We will take care of shipment of products right through your customer's door steps.  Free Business and leadership training from the company.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                <div class="col-md-6 mb-3">
+                    <div class="media landing-feature">
+                        <span class="landing-feature-icon"></span>
+                        <div class="media-body">
+                            <h5>Premium</h5>
+                            <p>All access to website will be unlocked including the e commerce which you will have your own shop and Real time after your customer paid the products it will reflect on your dashboard. Anytime 24/7 you can cashout your earnings and it will reflect to your bank account 24-48 hours.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                <div class="col-md-6 mb-3">
+                    <div class="media landing-feature">
+                        <span class="landing-feature-icon"></span>
+                        <div class="media-body">
+                            <h5>Training</h5>
+                            <p>A Free Business and leadership training from the company. You will learn how to grow as a businessman / woman and how to grow your business online.</p>
+                        </div>
+                        </div>
+                        </div>
+
+                <div class="col-md-6 mb-3">
+                    <div class="media landing-feature">
+                        <span class="landing-feature-icon"></span>
+                        <div class="media-body">
+                            <h5>Incentives</h5>
+                            <p>You will be entitled to achieve any of the company's bonuses including travel , gadgets , car , and even house and lot.</p>
+                        </div>
+                        </div>
+                        </div>
+            </div>
+        </div>
+   
+    
+    <section class="py-5 mb-3">
+        <div class="container">
+            <h2>FAQ</h2>
+            <p class="section-subtitle">Frequently Asked Questions</p>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card mb-3 landing-faq-card">
+                        <div class="card-header bg-white" id="faqOneTitle">
+                            <a href="#faqOneCollapse" class="d-flex align-items-center" data-toggle="collapse">
+                                <h6 class="mb-0">What is Vestige Infinity?</h6> <i class="far fa-plus-square ml-auto"></i>
+                            </a>
+                        </div>
+                        <div id="faqOneCollapse" class="collapse" aria-labelledby="faqOneTitle">
+                            <div class="card-body">
+                                <p class="mb-0 text-gray">Vestige infinity is a company that helps you earn even at home. The company will take care of your exclusive E commerce business specifically vestige infinity products. It's the company that will help you grow and become your own version of CEO at your most comfortable way.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 landing-faq-card">
+                        <div class="card-header bg-white" id="faqTwoTitle">
+                            <a href="#faqTwoCollapse" class="d-flex align-items-center" data-toggle="collapse">
+                                <h6 class="mb-0">Why should I use Vestige Infinity?</h6> <i class="far fa-plus-square ml-auto"></i>
+                            </a>
+                        </div>
+                        <div id="faqTwoCollapse" class="collapse" aria-labelledby="faqTwoTitle">
+                            <div class="card-body">
+                                <p class="mb-0 text-gray">If you wanted to have extra income even at home you should become part of vestige infinity family.
+                                    Vestige infinity website is very user-friendly. Your account here is secured and also your income. You can also request your earnings anytime 24/7 and wait for it in your bank for only 24-48 hours only. It also has customer service that you can talk that are direct from the company personnel.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    </div>
+              
+                <div class="col-lg-6">
+                   
+                
+                    <div class="card mb-3 landing-faq-card">
+                        <div class="card-header bg-white" id="faqSixTitle">
+                            <a href="#faqSixCollapse" class="d-flex align-items-center" data-toggle="collapse">
+                                <h6 class="mb-0">Being a user, what all rights I have?</h6> <i class="far fa-plus-square ml-auto"></i>
+                            </a>
+                        </div>
+                        <div id="faqSixCollapse" class="collapse" aria-labelledby="faqSixTitle">
+                            <div class="card-body">
+                                <p class="mb-0 text-gray">As a user you have all the right to access your account and upgrade it anytime you want. You also have the right to encash your encashment anytime. If, your account is already upgraded, you have the right to join at any trainings for free, you will also have all the access from the system. You will be joined in our official group and pages. You will also be notified for updates and incentives from the company. You will also be entitled to have 25-50% discount. All website features will be unlocked, and you will become one of vestige infinity official distributor.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mb-3 landing-faq-card">
+                        <div class="card-header bg-white" id="faqThreeTitle">
+                            <a href="#faqThreeCollapse" class="d-flex align-items-center" data-toggle="collapse">
+                                <h6 class="mb-0">How can I use Vestige Infinity?</h6> <i class="far fa-plus-square ml-auto"></i>
+                            </a>
+                        </div>
+                        <div id="faqThreeCollapse" class="collapse" aria-labelledby="faqThreeTitle">
+                            <div class="card-body">
+                                <p class="mb-0 text-gray">All you need to do is go to the website www.vestigeinfinity.com and click log in to log in your account. If you wanted to sign up just click sign up and you will be directed to our customer service personnel in our official FB page. They will give you a link as to where you can sign up and also instructions.</p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+            <footer class="border-top">
+            <br>
+                <p class="text-center text-muted pt-4">Copyright © 2020<a href="https://www.facebook.com/vestigeinfinity/" class="px-1">@vestigeinfinity</a>All rights reserved.</p>
+            <br>
+            </footer>
+    <script src="assets/vendors/jquery/jquery.min.js"></script>
+    <script src="assets/vendors/popper.js/popper.min.js"></script>
+    <script src="assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
